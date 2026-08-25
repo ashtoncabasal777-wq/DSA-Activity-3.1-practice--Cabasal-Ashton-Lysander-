313d63df661dab3b168e827ef6e9e47c7a8ad3e0
AshtonCabasal <AshtonCabasal@SMU-0469>
2026-08-25 10:00:35 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+string[] months = new string[12];
+for (int i = 0; i < months.Length; i++)
+{
+    months[i] = new DateTime(DateTime.Now.Year, month, 1);
+    string monthName = months[i].ToString("MMMM");
+
+}
