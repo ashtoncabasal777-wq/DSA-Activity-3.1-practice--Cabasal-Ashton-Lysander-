399458856c1ec79c85cd92ec3ea0e1dc003e04d0
ashtoncabasal777-wq <ashtoncabasal777@gmail.com>
2026-08-25 10:23:28 +0800

    some changes of codes

--- Program.cs
+++ Program.cs
@@ -1,17 +1,65 @@
-﻿
-using System.Globalization;
-
-string[] months new string[12];
-for (int month = 1; month <= 12; month++)
+﻿using System;
+namespace ConsoleApp2
 {
-    DateTime firstDay = new DateTime(DateTime.Now.Year, month, 1); string name firstDay.ToString("MMMM",
-    CultureInfo.CreateSpecificCulture("en"));
-    months[month - 1] = name;
-}
-foreach (string month in months)
-{
-    Console.WriteLine($"-> [month}");
-}
-Console.ReadKey();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * DSA Activity 3.1 - [Cabasal, Ashton Lysander]
+             * Program Description: Array of Scores
+             * Accept 10 scores, display the scores,
+             * calculate the average, and display a remark.
+             */
+            //some changes of code
 
-// some changes
+            int[] scores = new int[10];
+            int sum = 0;
+
+            // Input 10 scores
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter score " + (i + 1) + ": ");
+                scores[i] = Convert.ToInt32(Console.ReadLine());
+                sum += scores[i];
+            }
+
+            // Display scores
+            Console.WriteLine();
+            Console.Write("Scores: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(scores[i] + " ");
+            }
+
+            // Calculate average
+            int average = sum / 10;
+
+            Console.WriteLine();
+            Console.WriteLine("Average score is " + average);
+
+            // Display remark
+            string remark;
+
+            if (average >= 4)
+            {
+                remark = "Excellent";
+            }
+            else if (average >= 3)
+            {
+                remark = "Good";
+            }
+            else if (average >= 2)
+            {
+                remark = "Fair";
+            }
+            else
+            {
+                remark = "Poor";
+
+                Console.WriteLine("Remark: " + remark);
+            }
+        }
+    }
+}
